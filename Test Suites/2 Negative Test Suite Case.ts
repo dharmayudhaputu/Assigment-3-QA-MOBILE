e38d4f6e65decf459c61805435c67c7fbde2314a
dharmayudhaputu <dharmayudhaputu@gmail.com>
2024-02-13 11:19:07 +0800

    commit 7 - penambahan test suite positive test case

---
+++ Test Suites/2 Negative Test Suite Case.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>2 Negative Test Suite Case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ac82d7d5-d04d-412a-a7be-32529244de6e</testSuiteGuid>
+   <testCaseLink>
+      <guid>b586d850-0f53-4723-bcf8-9d771223c960</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/6 Negatif Case - Add to Chart</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dd41b85d-583b-441d-b6e7-7c55e3205c56</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/7 Negatif Case - Invalid Search</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
